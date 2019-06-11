330ce916b806fee49f5805dd59f3c45c765b9a32
Jelncy <jelncy118@163.com>
2019-06-11 17:30:46 +0800

    babel-loader&style-loader用法

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import './css/common.css';
+import layer from './components/layer/layer.js';
+
+
+const App = function (){
+    const NUM = 1;
+    alert(NUM);
+    console.log(layer);
+}
+
+new App()
--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,24 @@ var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   /*entry: ['./src/index.js', './src/test.js'],*/
-  entry: {
+  /* entry: {
     main: './src/script/index.js',
     test: './src/script/test.js',
     a: './src/script/a.js',
     b: './src/script/b.js',
     c: './src/script/c.js'
-  },
+  }, */
+  context: __dirname,
+  entry: ['babel-polyfill', './src/app.js'],
   output: {
-    filename: 'js/[name].js',
+    filename: 'js/[name].bundle.js',
     chunkFilename: '[id].js',
     path: path.resolve(__dirname, 'dist'),
     //path: './dist',
     // publicPath: 'http://cdn.com/'
   },
   plugins: [
-    new htmlWebpackPlugin({
+    /* new htmlWebpackPlugin({
       template: './src/index.html',
       //filename: 'index-[hash].html',
       filename: 'index.html',
@@ -71,13 +73,48 @@ module.exports = {
         collapseWhitespace: false,//true 删除空格，false 保留
       },
       chunks: ['c']
+    }) */
+
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: 'body'
     })
   ],
   mode: 'development',
   module: {
     rules: [
-      { test: /\.css$/, use: ['style-loader','css-loader'] },
-      { test: /\.ts$/, use: ['ts-loader']}
+      { 
+        test: /\.css$/, 
+        //style-loader!css-loader  style-loader要在css-loader前面
+        // style-loader!css-loader!postcss-loader   
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {importLoaders: 1}  //importLoaders的用法
+          },
+         'postcss-loader'
+        ]  
+      },
+      { 
+        test: /\.ts$/, 
+        use: ['ts-loader']
+      },
+      { 
+        test: /\.js$/, 
+        include: path.resolve(__dirname, 'src'),
+        exclude: path.resolve(__dirname, 'node_modules'), 
+        use: {
+          loader: 'babel-loader',
+          options: {  //注：loader+options写法
+            babelrc: false,
+            presets: ['@babel/preset-env']
+          }
+        }
+        /* "babel": {
+          "presets": ["@babel/preset-env"]
+        },  // 此处是写在package.json中的配置，两处都可以配置，也可以新建.babelrc 文件，在该文件中配置*/
+      }
     ]
   }
 };
